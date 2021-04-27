ba608b3f59e2177c86b359567d3084967fe62329
StDimensiy <stdimensiy@yandex.ru>
2021-04-27 13:09:53 +0300

    Initial commit

---
+++ src/main/java/TestTaskJavaSQL.java
@@ -0,0 +1,11 @@
+/**
+ * @Author Dmitry Veremeenko aka StDimensiy
+ * Test Work for dbeaver.com
+ * Created 27.04.2021
+ * v1.0
+ */
+public class TestTaskJavaSQL {
+    public static void main(String[] args) {
+
+    }
+}
